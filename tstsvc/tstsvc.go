87625d163579100a6a56121f82819fd1aef56040
jayven <kassarar@gmail.com>
2018-10-27 09:17:00 +0800

    Add RandPort

--- tstsvc/tstsvc.go
+++ tstsvc/tstsvc.go
@@ -2,6 +2,8 @@ package tstsvc
 
 import (
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/ory/dockertest"
 )
@@ -16,9 +18,16 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	rand.Seed(time.Now().UnixNano())
 }
 
 // DefaultPool returns the default dockertest Pool.
 func DefaultPool() *dockertest.Pool {
 	return defaultPool
+}
+
+// RandPort returns a random port number.
+func RandPort() uint16 {
+	return 50000 + uint16(rand.Int()%14983)
 }
---
+++ tstsvc/tstsvc_test.go
@@ -0,0 +1,12 @@
+package tstsvc
+
+import (
+	"log"
+	"testing"
+)
+
+func TestRandPort(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		log.Println(RandPort())
+	}
+}
