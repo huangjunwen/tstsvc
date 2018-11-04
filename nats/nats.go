882ae4c984391d65d149d71a779ac2f71f6701de
jayven <kassarar@gmail.com>
2018-11-04 20:19:03 +0800

    Add tstnats

---
+++ nats/nats.go
@@ -0,0 +1,153 @@
+package tstnats
+
+import (
+	"fmt"
+
+	"github.com/huangjunwen/tstsvc"
+
+	nats "github.com/nats-io/go-nats"
+	"github.com/ory/dockertest"
+	dc "github.com/ory/dockertest/docker"
+)
+
+var (
+	// Docker repository.
+	Repository = "nats"
+
+	// Default tag.
+	DefaultTag = "1.3.0-linux"
+
+	// Default container expire time.
+	DefaultExpire uint = 120
+)
+
+var (
+	// Default options.
+	defaultOptions = &Options{}
+)
+
+// Resource represents a test nats server.
+type Resource struct {
+	// Nats streaming server docker container.
+	*dockertest.Resource
+
+	// Actual options.
+	Options
+}
+
+// Options is options to run a test nats server.
+type Options struct {
+	// Tag of the repository. Default: DefaultTag.
+	Tag string
+
+	// If specified, the port 4222/tcp will be mapped to it. Default: random port.
+	HostPort uint16
+
+	// If specified, the port 8222/tcp will be mapped to it. Default: random port.
+	HostMonPort uint16
+
+	// If specified, the port 6222/tcp will be mapped to it. Default: random port.
+	HostClusterPort uint16
+
+	// Expire time (in seconds) of the container. Default: DefaultExpire.
+	Expire uint
+}
+
+// Run is equivalent to RunFromPool(nil, opts).
+func Run(opts *Options) (*Resource, error) {
+	return RunFromPool(nil, opts)
+}
+
+// RunFromPool runs a test nats server. If pool is nil, tstsvc.DefaultPool() will be used.
+// If opts is nil, the default options will be used.
+func RunFromPool(pool *dockertest.Pool, opts *Options) (*Resource, error) {
+	// Handle nil case.
+	if pool == nil {
+		pool = tstsvc.DefaultPool()
+	}
+	if opts == nil {
+		opts = defaultOptions
+	}
+
+	// Collect options.
+	res := &Resource{
+		Options: *opts,
+	}
+	opts = &res.Options
+
+	if opts.Tag == "" {
+		opts.Tag = DefaultTag
+	}
+	if opts.HostPort == 0 {
+		opts.HostPort = tstsvc.FreePort()
+	}
+	if opts.HostMonPort == 0 {
+		opts.HostMonPort = tstsvc.FreePort()
+	}
+	if opts.HostClusterPort == 0 {
+		opts.HostClusterPort = tstsvc.FreePort()
+	}
+	if opts.Expire == 0 {
+		opts.Expire = DefaultExpire
+	}
+
+	// Run the container.
+	runOpts := &dockertest.RunOptions{
+		Repository: Repository,
+		Tag:        opts.Tag,
+		PortBindings: map[dc.Port][]dc.PortBinding{
+			"4222/tcp": []dc.PortBinding{
+				dc.PortBinding{
+					HostIP:   "localhost",
+					HostPort: fmt.Sprintf("%d", opts.HostPort),
+				},
+			},
+			"8222/tcp": []dc.PortBinding{
+				dc.PortBinding{
+					HostIP:   "localhost",
+					HostPort: fmt.Sprintf("%d", opts.HostMonPort),
+				},
+			},
+			"6222/tcp": []dc.PortBinding{
+				dc.PortBinding{
+					HostIP:   "localhost",
+					HostPort: fmt.Sprintf("%d", opts.HostClusterPort),
+				},
+			},
+		},
+	}
+
+	var err error
+	res.Resource, err = pool.RunWithOptions(runOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set expire of the container.
+	res.Resource.Expire(opts.Expire)
+
+	// Wait.
+	if err := pool.Retry(func() error {
+		nc, err := nats.Connect(res.NatsURL())
+		if err != nil {
+			return err
+		}
+		nc.Close()
+		return nil
+	}); err != nil {
+		res.Close()
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// NatsURL returns the nats url to connect to the nats streaming server.
+func (res *Resource) NatsURL() string {
+	return fmt.Sprintf("nats://localhost:%d", res.Options.HostPort)
+}
+
+// NatsClient returns a nats client of the embedded nats server of the test nats streaming server.
+func (res *Resource) NatsClient(opts ...nats.Option) (*nats.Conn, error) {
+	return nats.Connect(res.NatsURL(), opts...)
+}
---
+++ nats/nats_test.go
@@ -0,0 +1,93 @@
+package tstnats
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/huangjunwen/tstsvc"
+
+	nats "github.com/nats-io/go-nats"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	subject = "tstnatsSubject"
+)
+
+func TestRun(t *testing.T) {
+	assert := assert.New(t)
+	var err error
+
+	opts := &Options{
+		HostPort:        tstsvc.FreePort(),
+		HostMonPort:     tstsvc.FreePort(),
+		HostClusterPort: tstsvc.FreePort(),
+	}
+	log.Printf("%#v\n", *opts)
+
+	// Run the first server.
+	var res1 *Resource
+	{
+		res1, err = Run(opts)
+		assert.NoError(err)
+		defer res1.Close()
+	}
+	log.Printf("The first nats server is up, nats url: %+q.\n", res1.NatsURL())
+	log.Printf("%#v\n", res1.Options)
+
+	// Make the connection.
+	var nc *nats.Conn
+	{
+		nc, err = res1.NatsClient(
+			nats.MaxReconnects(-1), // Never give up reconnect.
+			nats.ReconnectWait(100*time.Millisecond),
+		)
+		assert.NoError(err)
+		defer nc.Close()
+	}
+	log.Printf("Connected to the first nats server.\n")
+
+	// Subscribe.
+	handlerc := make(chan struct{})
+	{
+		_, err := nc.Subscribe(subject, func(m *nats.Msg) {
+			handlerc <- struct{}{}
+		})
+		assert.NoError(err)
+	}
+	log.Printf("Subscribed to %+q.\n", subject)
+
+	// Publish.
+	{
+		err := nc.Publish(subject, []byte("good"))
+		assert.NoError(err)
+		<-handlerc
+	}
+	log.Printf("Publishd to %+q and handled.\n", subject)
+
+	// Stop the first server.
+	res1.Close()
+	log.Printf("The first nats server is down.\n")
+
+	// Run the second server.
+	var res2 *Resource
+	{
+		res2, err = Run(opts)
+		assert.NoError(err)
+		defer res2.Close()
+	}
+	log.Printf("The second nats server is up, nats url: %+q.\n", res2.NatsURL())
+	log.Printf("%#v\n", res2.Options)
+
+	// Wait a while.
+	time.Sleep(time.Second)
+
+	// Publish again.
+	{
+		err := nc.Publish(subject, []byte("good"))
+		assert.NoError(err)
+		<-handlerc
+	}
+	log.Printf("Publishd again to %+q and handled.\n", subject)
+}
